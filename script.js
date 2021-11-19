1c7b7e0406f52668e818623cbb6a2adcf1fdbbb5
ChrisM6 <>
2021-11-19 17:41:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+hi guys can we discuss scheduling rq
+
+yeah for me im probs gonna be a bit busier over like the next two weeks due to internship stuff. 
+
+what would help is if we can shift the time backwards a little (like 9:00pm-12:00pm). I'd probably leave at 11ish to sleep tho.
+
+Haha yeah originally when we created the schedule it was just my schedule I used for ao. It was kinda random.
+
+We shaved some time off, and reorganized it a bit, but uh yeah now that we have new members and stuff we might wanna reorganize
+
+
+
+ */
