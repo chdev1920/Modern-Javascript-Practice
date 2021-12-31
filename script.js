12a95f57ca5207321e4fabdf186394d59df18dce
Chris M <96895326+chdev1920@users.noreply.github.com>
2021-12-31 03:07:06 +0000

    testing if statements and control flow

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-//numbers
-let dataOne = 1;
+let password = "OpenSeasameOrSomething99"
 
-//string 
-let dataTwo = 'hello world';
-
-//Null (no value)
-let dataThree = null;
-
-//Undefined (not yet defined)
-let dataFour = undefined;
-let dataFourExample;
-
-//Object
-let dataFive = ["arrays", "dates", "literals"];
-
-//Symbols (used with objects)
-let dataSix = Symbol();
-
-console.log(dataOne, dataTwo, dataThree, dataFour, dataFive, dataSix);
+if(password.length >= 10 
+  && (password.includes('1')
+  || password.includes('2')
+  || password.includes('3')
+  || password.includes('4')
+  || password.includes('5')
+  || password.includes('6')
+  || password.includes('7')
+  || password.includes('8')
+  || password.includes('9')
+  || password.includes('0'))){
+  console.log("it's strong");
+} else if(password.length >= 5){
+  console.log('not long enough');
+} else {
+  console.log('not long enough');
+}
